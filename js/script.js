ec9788dd139c235ca96b3712163c22854084988a
shriira-it <admin@shriira.one>
2025-03-26 16:03:56 +0530

    more

--- js/script.js
+++ js/script.js
@@ -213,3 +213,212 @@ const myFunction7 = item => "<li>" + item + "</li>";
 let newArray = backpackContents.map(item => "<li>" + item + "</li>");
 
 console.log("newArray:", newArray);
+
+
+function doSomething(a, b) {
+    return a + b;
+};
+
+console.log("doSomething:", doSomething(1, 2));
+
+const doSomeOtherThing = function (a, b) {
+    return a + b;
+}
+
+console.log("doSomeOtherThing: ", doSomeOtherThing(1, 3));
+
+const myArrowFunction = (a, b, c) => {
+    a = a + c;
+    b = b * c;
+    return a + b + c;
+};
+
+console.log("myArrowFunction:", myArrowFunction(2,3,4));
+
+
+const myOptimizedArrowFunction = (a, b, c) => (a + b + c);
+    
+console.log("immediatelyInvokedFunction:", ((a, b, c) => {
+    a = a + c;
+    b = b * c;
+    return a + b + c;
+})(1, 5, 7));
+
+
+const greenPack = {
+    name: "Frog Pack",
+    color: "green",
+    volume: 8,
+    numOfPockets: 3,
+    newVolume: function (volume) {
+        this.volume = volume;
+        // (function () {
+        //     console.log("this.volume in the nested function", 
+        //         this.volume);
+        // })()
+        (() => {
+            console.log("this.volume in the nested function", 
+                        this.volume);
+        })();
+    }
+}
+greenPack.newVolume(40)
+console.log("greenPack.volume:", greenPack.volume);
+
+function myMainFunction(a, b, c, anyCallbackFunction) {
+    a = a + b + c;
+    b = b + c + a;
+    c = c + a + b;
+
+    anyCallbackFunction(a + b + c);
+}
+
+function myGenericFunction(result) {
+    console.log("result: ", result);
+};
+
+myMainFunction(2, 3, 4, myGenericFunction);
+
+myMainFunction(3, 4, 6, (r) => {
+    console.log("r:", r);
+});
+
+
+myMainFunction(3, 4, 6, function (r) {
+    console.log("anonymous r:", r);
+});
+
+
+// Conditional Statements if, else, switch
+
+// if (condition) {
+//     // if block
+// } else {
+//     // else block
+// }
+
+
+
+// >, >=, <, <=, ==, ===, !=, !==
+a = 5;
+b = 10;
+
+c = "5"
+
+console.log("a == c", a == c); // true
+console.log("a != c", a != c); // false
+
+console.log("a === c", a === c); // false
+console.log("a !== c", a !== c); // true
+
+if (a > 5) {
+    console.log("a is greater than 5");
+} else if (b >= a) {
+    console.log("b is greater than or equal to b");
+} else {
+    console.log("no conditions have been satisfied.");
+}
+
+age = 70;
+
+switch (true) {
+    case age < 30:
+        console.log("age < 30");
+        break;
+    case age < 50:
+        console.log("age >=30 and < 50");
+        break;
+    case age < 60:
+    case age < 55:
+        console.log("age > 50");
+        console.log("age < 60");
+        console.log("age < 55");
+        break;
+    default:
+        console.log("did not match any case");
+}
+
+
+a = 10
+console.log("a:", a);
+a = a + 1;
+a++
+console.log("a", a);
+b = a++
+console.log("b:", b)
+console.log("a:", a);
+
+c = ++a
+console.log("c:", c);
+console.log("a:", a);
+// console.log("a:", a++);
+
+a--
+console.log("a:", a);
+d = a--
+console.log("d:", d);
+console.log("a:", a);
+
+d = --a
+console.log("d:", d);
+console.log("a:", a);
+
+
+for (let i=0; i < 10; i++) {
+    console.log("i:", i);
+}
+
+let stuff = ["a", "b", "c", "d", "e"]
+console.log("stuff.length", stuff.length);
+console.log("stuff[3]:", stuff[3])
+
+for (let i=0; i < stuff.length; i++) {
+    console.log(stuff[i]);
+}
+
+for (const item of stuff) {
+    console.log("item:", item);
+}
+
+stuff.forEach((item) => {
+    console.log("forEach item:", item);
+})
+
+let myNewArray = [];
+
+let forEachReturn = stuff.forEach((item) => {
+    myNewArray.push("<li>"+item+"</li>");
+})
+
+console.log("forEachReturn:", forEachReturn);
+
+console.log("myNewArray:", myNewArray);
+
+
+const myNestedObject = {
+    item01: {
+        name: "Piggy",
+        type: "toy",
+        weigth: 30
+    },
+    item02: {
+        name: "headlamp",
+        type: "equipment",
+        weight: 120
+    }
+}
+
+for (const myObject in myNestedObject) {
+    console.log(myObject);
+    console.log(myNestedObject[myObject]);
+}
+
+
+
+let myMappedArray = stuff.map((item) => {
+    console.log("mapItem:", item);
+    return "<li>"+item+"</li>";
+})
+
+console.log("myMappedArray:", myMappedArray);
+
