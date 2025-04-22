d6bbd22d323e83fc11164bdc3d3a8ff822c62b03
shriira-it <admin@shriira.one>
2025-04-22 17:34:07 +0530

    react hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {useTrees} from "./";
+
+function App() {
+  const {trees} = useTrees();
+  return (
+    <div>
+      <h1>Trees I've Heard Of</h1>
+      <ul>
+        {trees.map((tree) => (
+          <li key={tree.id}>{tree.type}</li>
+        )) }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
