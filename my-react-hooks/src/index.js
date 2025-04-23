6e546329ebf642971a8b98c807340bd17da9313e
shriira-it <admin@shriira.one>
2025-04-23 16:49:53 +0530

    wip

---
+++ my-react-hooks/src/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useReducer, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+
+const initialState = {
+  message: "hi"
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "yell":
+        return {message: `HEY! I JUST SAID ${state.message}`}
+    case "whisper":
+        return {message: `excuse me, I just said ${state.message}`}
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <p>Message: {state.message} </p>
+      <button onClick={() => dispatch({type: "yell"})}>YELL</button>
+      <button onClick={() => dispatch({type: "whisper"})}>Whisper</button>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
