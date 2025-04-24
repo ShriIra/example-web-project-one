a0ec474d5f53639e242aefcca3cc9933268b3560
shriira-it <admin@shriira.one>
2025-04-24 16:59:19 +0530

    dispatch

--- my-news-app/src/Components/App.js
+++ my-news-app/src/Components/App.js
@@ -1,38 +1,36 @@
-import {Component} from "react";
-import News from "./News"
-import './App.css';
+import { Component, useReducer } from "react";
+import News from "./News";
+import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      news1: {
-        category: "top-headlines",
-        query: "sources=techcrunch"
-      },
-
-      news2: {
-        category: "everything",
-        query: "domains=wsj.com"
-      },
-       news3: {
-        category: "everything",
-        query: "q=tesla&from=2025-03-17&sortBy=publishedAt"
-       }
+export default function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "everything":
+        return {category: "everything", query: "domains=wsj.com"};
+      case "tesla":
+        return {category: "everything", query: "q=tesla&sortBy=publishedAt"};
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        <News news={this.state.news1} />
-        <hr/>
-        <News news={this.state.news2} />
-        <hr/>
-        <News news={this.state.news3} />
-      </div>
-    );
-  }
-}
+  const [state, dispatch] = useReducer(reducer, {
+    category: "top-headlines",
+    query: "sources=techcrunch",
+  });
 
-export default App;
+  return (
+    <div className="App">
+      {/* <News news={this.state.news1} />
+      <hr/>
+      <News news={this.state.news2} />
+      <hr/>
+      <News news={this.state.news3} /> */}
+      <button onClick={() => dispatch({ type: "everything" })}>
+        everything
+      </button>
+      <button onClick={() => dispatch({ type: "tesla" })}>
+        tesla
+      </button>
+      <News news={state} />
+    </div>
+  );
+}
--- my-news-app/src/Components/News.js
+++ my-news-app/src/Components/News.js
@@ -1,54 +1,25 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Article from "./Article";
 import Error from "./Error";
 
-class News extends Component {
+export default function News({news}) {
+    const [data, setData] = useState({});
+    const [error, setError] = useState(null);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: [],
-            error: false,
-        }
+    useEffect(() => {
+        fetch(`https://newsapi.org/v2/${news.category}?${news.query}&apiKey=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => response.json())
+        .then(setData)
+        .catch(setError);
+    }, [news])
+
+    if (error) {
+        return <Error />
     }
-
-    componentDidMount() {
-        // fetch data
-        fetch(`https://newsapi.org/v2/${this.props.news.category}?${this.props.news.query}&apiKey=${process.env.REACT_APP_API_KEY}`).then((response) => {
-            return response.json();
-        }).then((data) => {
-
-            if (data.status === "error") {
-                this.setState({
-                    articles: [],
-                    error: true,
-                })
-                return
-            }
-            console.log(data);
-            this.setState({
-               articles: data.articles,
-            })
-        }).catch((error) => {
-            this.setState({
-                articles: [],
-                error: true,
-            })
-        });
-    }
-
-    render() {
-        if (this.state.error) {
-            return <Error />
-        } else {
-            return (
-                <div>
-                    {this.state.articles?.map((article) => <Article key={article.url} item={article} />)}
-                </div>
-            )
-        }
-        
-    }
+    return (
+        <div>
+            {data.articles?.map((article) => <Article key={article.url} item={article} />)}
+        </div>
+    )
+    
 }
-
-export default News;
